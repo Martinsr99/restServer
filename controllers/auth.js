20d9b8006380ed4108d1e63f540c978fa6d205f4
Martin Siles <silesreche.martin@gmail.com>
2022-09-01 16:14:19 +0200

    Add google verification lo sign in and sign out

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const bcryptjs = require("bcryptjs");
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generarJWT");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 
@@ -56,4 +57,53 @@ const login = async (req, res = response) => {
   }
 };
 
-module.exports = { login };
+
+const googleSignIn = async (req, res=response) => {
+
+  const {id_token} = req.body;
+
+  try {
+    const {nombre,img,correo} = await googleVerify(id_token)
+    let usuario = await Usuario.findOne({correo})
+
+    if(!usuario){
+      const data = {
+        nombre,
+        correo,
+        password: '123456',
+        img,
+        rol:'ADMIN_ROLE',
+        google:true
+      }
+
+      usuario = new Usuario(data);
+      await usuario.save()
+    }
+
+    //Si el usuario en DB
+
+    if(!usuario.estado) {
+      return res.status(401).json({
+        msg: 'Hable con el administrador, usuario bloqueado'
+      })
+    }
+
+    //Generar JWT
+    const token = await generarJWT (usuario.id)
+
+    res.json({
+      usuario,
+      token
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({
+      ok:false,
+      msg:'El token no se pudo verificar'
+    })
+  }
+
+
+}
+
+module.exports = { login,googleSignIn };
